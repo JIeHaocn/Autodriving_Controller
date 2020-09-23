78aa442cb6664ee901ca9430d56ff84971f2f683
JIeHaocn <JIeHaocn@users.noreply.github.com>
2020-09-23 18:28:52 +0800

    Addition

---
+++ Create_trajectory_points/Create_Trajectory.m
@@ -0,0 +1,136 @@
+classdef Create_Trajectory
+    %UNTITLED3 此处显示有关此类的摘要
+    %   此处显示详细说明
+    
+    properties
+        sample_time = 0.1   % unit: s
+        velocity = 0.5      % unit: m/s
+        x0 = 0               % unit: m
+        y0 = 0               % unit: m
+        theta0 = 0           % unit: degree    
+        traj_x
+        traj_y
+        traj_theta
+        traj_curvature
+        traj_speed
+    end
+    properties
+        Lw = 0.256
+        Lf = 0.07 
+        Lr = 0.05
+        W = 0.20
+        Tire_length = 0.066
+        Tire_width_f = 0.026
+        Tire_width_r = 0.03
+        Trans_mat
+        Tire_Trans_mat  
+    end
+    
+    methods
+        function obj = Create_Trajectory(sample_time,velocity,x0,y0,theta0)
+            obj.sample_time = sample_time;
+            obj.velocity = velocity;
+            obj.x0 = x0;
+            obj.y0 = y0;
+            obj.theta0 = theta0/180*pi;
+            obj.Trans_mat = [ obj.Lw + obj.Lf,  obj.W/2, 1; ...
+                              obj.Lw + obj.Lf, -obj.W/2, 1; ...
+                                      -obj.Lr, -obj.W/2, 1; ...
+                                      -obj.Lr,  obj.W/2, 1];
+            obj.Tire_Trans_mat =  [ obj.Lw + obj.Tire_length/2,  obj.W/2 + obj.Tire_width_f/2, 1; ...
+                                    obj.Lw + obj.Tire_length/2,  obj.W/2 - obj.Tire_width_f/2, 1; ...
+                                    obj.Lw - obj.Tire_length/2,  obj.W/2 - obj.Tire_width_f/2, 1; ...
+                                    obj.Lw - obj.Tire_length/2,  obj.W/2 + obj.Tire_width_f/2, 1; ...
+                                    obj.Lw + obj.Tire_length/2, -obj.W/2 + obj.Tire_width_f/2, 1; ...
+                                    obj.Lw + obj.Tire_length/2, -obj.W/2 - obj.Tire_width_f/2, 1; ...
+                                    obj.Lw - obj.Tire_length/2, -obj.W/2 - obj.Tire_width_f/2, 1; ...
+                                    obj.Lw - obj.Tire_length/2, -obj.W/2 + obj.Tire_width_f/2, 1; ...
+                                             obj.Tire_length/2, -obj.W/2 + obj.Tire_width_r/2, 1; ...
+                                             obj.Tire_length/2, -obj.W/2 - obj.Tire_width_r/2, 1; ...
+                                            -obj.Tire_length/2, -obj.W/2 - obj.Tire_width_r/2, 1; ...
+                                            -obj.Tire_length/2, -obj.W/2 + obj.Tire_width_r/2, 1; ...
+                                             obj.Tire_length/2,  obj.W/2 + obj.Tire_width_r/2, 1; ...
+                                             obj.Tire_length/2,  obj.W/2 - obj.Tire_width_r/2, 1; ...
+                                            -obj.Tire_length/2,  obj.W/2 - obj.Tire_width_r/2, 1; ...
+                                            -obj.Tire_length/2,  obj.W/2 + obj.Tire_width_r/2, 1];                    
+        end
+        
+        function obj = add_line(obj,length)
+            if isempty(obj.traj_x) 
+                [traj_x_temp, traj_y_temp, traj_theta_temp] = get_line_points(obj.x0, obj.y0, obj.theta0, length, obj.velocity, obj.sample_time);
+            else
+                [traj_x_temp, traj_y_temp, traj_theta_temp] = get_line_points(obj.traj_x(end), obj.traj_y(end), obj.traj_theta(end), length, obj.velocity, obj.sample_time);
+            end
+            traj_curvature_temp = zeros(size(traj_x_temp,1),1);
+            traj_speed_temp =  zeros(size(traj_x_temp,1),1);
+            traj_speed_temp(:) = obj.velocity;
+            obj.traj_x = [obj.traj_x;traj_x_temp];
+            obj.traj_y = [obj.traj_y;traj_y_temp];
+            obj.traj_theta = [obj.traj_theta;traj_theta_temp];
+            obj.traj_curvature = [obj.traj_curvature;traj_curvature_temp];
+            obj.traj_speed = [obj.traj_speed;traj_speed_temp];
+        end
+        
+        function obj = add_circle(obj,theta_d,radius)
+            if isempty(obj.traj_x) 
+                [traj_x_temp, traj_y_temp, traj_theta_temp] = get_circle_points(obj.x0, obj.y0, obj.theta0, theta_d, radius, obj.velocity, obj.sample_time);
+            else
+                [traj_x_temp, traj_y_temp, traj_theta_temp] = get_circle_points(obj.traj_x(end), obj.traj_y(end), obj.traj_theta(end), theta_d, radius, obj.velocity, obj.sample_time);
+            end
+            traj_curvature_temp = zeros(size(traj_x_temp,1),1);
+            traj_curvature_temp(:) = 1/radius;
+            traj_speed_temp =  zeros(size(traj_x_temp,1),1);
+            traj_speed_temp(:) = obj.velocity;
+            obj.traj_x = [obj.traj_x;traj_x_temp];
+            obj.traj_y = [obj.traj_y;traj_y_temp];
+            obj.traj_theta = [obj.traj_theta;traj_theta_temp];
+            obj.traj_curvature = [obj.traj_curvature;traj_curvature_temp];
+            obj.traj_speed = [obj.traj_speed;traj_speed_temp];
+        end
+        
+        function plot_traj(obj)
+            % Plot the trajectory
+            figure('Name','Desired Trajectory')
+            plot(obj.traj_x, obj.traj_y);
+        end 
+        
+        function ani_traj(obj)
+            x_left = min(obj.traj_x)-0.5;
+            x_right = max(obj.traj_x)+0.5;
+            y_left = min(obj.traj_y)-0.5;
+            y_right = max(obj.traj_y)+0.5;
+            figure('Name','Trajectory Animation ')            
+            for i=1:size(obj.traj_x,1)             
+                plot(obj.traj_x(1:i), obj.traj_y(1:i),'-b');
+                hold on               
+                vertex_x = obj.Trans_mat*[cos(obj.traj_theta(i)); -sin(obj.traj_theta(i)); obj.traj_x(i)];
+                vertex_y = obj.Trans_mat*[sin(obj.traj_theta(i));  cos(obj.traj_theta(i)); obj.traj_y(i)];
+                plot([vertex_x(1) vertex_x(2)], [vertex_y(1) vertex_y(2)],'-k');
+                plot([vertex_x(2) vertex_x(3)], [vertex_y(2) vertex_y(3)],'-k');
+                plot([vertex_x(3) vertex_x(4)], [vertex_y(3) vertex_y(4)],'-k');
+                plot([vertex_x(4) vertex_x(1)], [vertex_y(4) vertex_y(1)],'-k');
+                tire_x = obj.Tire_Trans_mat*[cos(obj.traj_theta(i)); -sin(obj.traj_theta(i)); obj.traj_x(i)];
+                tire_y = obj.Tire_Trans_mat*[sin(obj.traj_theta(i));  cos(obj.traj_theta(i)); obj.traj_y(i)];  
+                for i_tire = 0:3
+                    plot([tire_x(i_tire*4+1) tire_x(i_tire*4+2)], [tire_y(i_tire*4+1) tire_y(i_tire*4+2)],'-k');
+                    plot([tire_x(i_tire*4+2) tire_x(i_tire*4+3)], [tire_y(i_tire*4+2) tire_y(i_tire*4+3)],'-k');
+                    plot([tire_x(i_tire*4+3) tire_x(i_tire*4+4)], [tire_y(i_tire*4+3) tire_y(i_tire*4+4)],'-k');
+                    plot([tire_x(i_tire*4+4) tire_x(i_tire*4+1)], [tire_y(i_tire*4+4) tire_y(i_tire*4+1)],'-k');
+                end
+                axis([x_left x_right y_left y_right])
+                pause(0.02);
+                hold off
+            end        
+        end
+        
+        function print_file(obj)
+%             traj_theta_deg = obj.traj_theta/pi*180; % unit: degree
+            fid=fopen('C:\Users\Jie Hao\Desktop\route.txt','w');    
+            for i=1:size(obj.traj_x,1)
+                fprintf(fid,'%.6f %.6f %.6f\n',obj.traj_x(i),obj.traj_y(i),obj.traj_speed(i));  
+            end
+        end
+        
+    end
+end
+
---
+++ Create_trajectory_points/Create_trajectory_main.m
@@ -0,0 +1,47 @@
+clear all
+close all
+sample_time = 0.1; % unit: s
+velocity = 0.5; % unit: m/s
+x0 = 0; % unit: m
+y0 = 0; % unit: m
+theta0 = 0; % unit: degree
+theta0 = theta0/180*pi;
+Car_Traj = Create_Trajectory(sample_time,velocity,x0,y0,theta0);
+
+length = 1.7;
+Car_Traj = Car_Traj.add_line(length);
+
+theta_d = pi; % unit: rad
+radius = 0.9; % unit: m
+Car_Traj = Car_Traj.add_circle(theta_d,radius);
+
+length = 2;
+Car_Traj = Car_Traj.add_line(length);
+
+theta_d = -pi/2; % unit: rad
+radius = 1.0; % unit: m
+Car_Traj = Car_Traj.add_circle(theta_d,radius);
+
+theta_d = -pi/3; % unit: rad
+radius = 1.2; % unit: m
+Car_Traj = Car_Traj.add_circle(theta_d,radius);
+
+length = 1.2;
+Car_Traj = Car_Traj.add_line(length);
+
+theta_d = pi/4; % unit: rad
+radius = 1.1; % unit: m
+Car_Traj = Car_Traj.add_circle(theta_d,radius);
+
+% Plot the trajectory
+Car_Traj.plot_traj;
+
+% Plot the trajectory
+Car_Traj.ani_traj;
+
+Car_Traj.print_file;
+traj_x = Car_Traj.traj_x;
+traj_y = Car_Traj.traj_x;
+traj_theta = Car_Traj.traj_x;
+traj_curvature = Car_Traj.traj_x;
+traj_speed = Car_Traj.traj_x;
